7881f4e1c42c28a3719555b9edfcdf02ab8a337e
PaulStoffregen <paul@pjrc.com>
2016-05-10 13:37:20 -0700

    Import Teensyduino patches

--- arduino-core/src/processing/app/Serial.java
+++ arduino-core/src/processing/app/Serial.java
@@ -109,6 +109,8 @@ public class Serial implements SerialPortEventListener {
     if (istopbits == 1.5f) stopbits = SerialPort.STOPBITS_1_5;
     if (istopbits == 2) stopbits = SerialPort.STOPBITS_2;
 
+    if (iname == "fake serial") return;
+
     try {
       port = new SerialPort(iname);
       port.openPort();
@@ -184,7 +186,7 @@ public class Serial implements SerialPortEventListener {
   }
 
 
-  private void write(byte bytes[]) {
+  public void write(byte bytes[]) {
     try {
       port.writeBytes(bytes);
     } catch (SerialPortException e) {
@@ -223,6 +225,26 @@ public class Serial implements SerialPortEventListener {
     } catch (SerialPortException e) {
       errorMessage("setRTS", e);
     }
+  }
+
+  public void setBaud(int rate) {
+    if (port == null) return;
+    try {
+      port.setParams(rate, 8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+    } catch (SerialPortException e) {
+    }
+  }
+  public boolean isOnline() {
+    if (port == null) return false;
+    if (!(port.isOpened())) return false;
+    boolean online;
+    try {
+      online = port.setDTR(true);
+    } catch (Exception e) {
+      online = false;
+    }
+    //System.out.println("set dtr, result = " + online);
+    return online;
   }
 
   static public List<String> list() {
